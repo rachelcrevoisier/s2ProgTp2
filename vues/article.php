4596fdff67cf2679bbe80ce02d870ecd04816dfc
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-01-05 10:06:14 -0500

    Creation de la page article

---
+++ vues/article.php
@@ -0,0 +1,20 @@
+<main>
+    <section class="article">
+        
+        <?php 
+            while($rangee = mysqli_fetch_assoc($lireArticle))
+            {
+                echo "<p class=\"date\">" . htmlspecialchars($rangee["date"]) . "</p>";
+                echo "<h1>" . htmlspecialchars($rangee["titre"]) . "</h1>";
+                echo "<img src=\"assets/img/articles/". htmlspecialchars($rangee["visuel"])."\" alt=\"". htmlspecialchars($rangee["titre"]) ."\">";
+                echo "<p>" . $rangee["texte"] . "</p>";
+                echo "<p class=\"auteur\">Journaliste : " . htmlspecialchars($rangee["journaliste"]) . "</p>";
+                echo "<div class=\"iconeEdition\">
+                <span class=\"material-symbols-outlined\"><a href=\"index.php?commande=supArticle&idArticle=".$rangee["articleId"]."\">delete</a></span>
+                <span class=\"material-symbols-outlined\"><a href=\"index.php?commande=formModifArticle&idArticle=".$rangee["articleId"]."\">edit_note</a></span>
+                </div>";
+            }
+        ?>
+    </section>
+    
+</main>
