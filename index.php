f3c587d2d1169882e17768b50cc54d2fcb98ba52
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-01-05 15:15:35 -0500

    Mise au propre du code

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php 
     /*  index.php = contrôleur 
         Il s'occupe des redirections et des contrôles
-    
     */
     //si le lien index contient ?commande= alors commande = le nom derrière le ?
     //$_REQUEST = par get ou post
@@ -27,38 +26,36 @@
             require_once("vues/header.php");
             require("vues/accueil.php");
             require_once("vues/footer.php");
-            break;
+        break;
         case "contact": 
             $titre = "contact";
             require_once("vues/header.php");
             require("vues/contact.php");
             require_once("vues/footer.php");
-            break; 
+        break; 
         case "quiSommesNous": 
             $titre = "Qui sommes Nous";
             require_once("vues/header.php");
             require("vues/quisommesnous.php");
             require_once("vues/footer.php");
-            break;  
-        case "recherche": 
-            $titre = "Rechercher";
-            require_once("vues/header.php");
-            require("vues/recherche.php");
-            require_once("vues/footer.php");
-            break; 
+        break;  
         case "seConnecter": 
             $titre = "Se connecter";
             require_once("vues/header.php");
             require("vues/connexion.php");
             require_once("vues/footer.php");
-            break; 
+        break; 
+        // Lecture d'un article
         case "article":
+            // On vérifie s'il y a un get ID et s'il est numérique. S'il y en a pas, on redirige vers index
             if(!isset($_GET["idArticle"]) || !is_numeric($_GET["idArticle"]))
             {
                 header("Location: index.php");
                 die();
             }
+            // On a besoin de la fonction articleID avec l'ID de l'article en paramètre
             $lireArticle = articleId($_GET["idArticle"]);
+            // Si on a un résultat, alors on va chercher les pages pour lire un article, autrement, on redirige vers index
             if(mysqli_num_rows($lireArticle) > 0)
             {
                 $titre = "Lire article";
@@ -72,14 +69,17 @@
                 header("Location: index.php");
                 die();
             }
-            break;
+        break;
         case "formModifArticle":
+            // On vérifie s'il y a un get ID et s'il est numérique. S'il y en a pas, on redirige vers index
             if(!isset($_GET["idArticle"]) || !is_numeric($_GET["idArticle"]))
             {
                 header("Location: index.php");
                 die();
             }
+            // On a besoin de la fonction articleID avec l'ID de l'article en paramètre
             $lireArticle = articleId($_GET["idArticle"]);
+            // Si on a un résultat, alors on va chercher les pages pour modifier un article, autrement, on redirige vers index
             if(mysqli_num_rows($lireArticle) > 0)
             {
                 $titre = "Modifier article";
@@ -92,8 +92,10 @@
                 header("Location: index.php");
                 die();
             }
-            break;
+        break;
+        // On modifie l'article
         case "modifieArticle":
+            //Si on a bien tous nos request, on supprime les espaces et ensuite on vérifie s'ils ne sont pas vides, si pas ok, on redirige vers le formulaire avec l'id en parametre
             if(isset($_REQUEST["date"], $_REQUEST["titre"], $_REQUEST["texte"], $_REQUEST["visuel"], $_REQUEST["id"], $_REQUEST["rubrique"]))
             { 
                 $date = trim($_REQUEST["date"]);
@@ -102,36 +104,35 @@
                 $visuel = trim($_REQUEST["visuel"]);
                 $idArticle = $_REQUEST["id"];
                 $rubrique = $_REQUEST["rubrique"];
-                    if($date != "" && $titre != "" && $texte != ""&& $rubrique != "" && $visuel != "" && is_numeric($idArticle))
-                    { 
-                        $resultat = modifieArticle($date, $titre, $texte, $visuel, $idArticle, $rubrique);
-                        $lireArticle = articleId($idArticle);
-                        if(mysqli_num_rows($lireArticle) > 0)
-                        {
-                            $titre = "Lire article";
-                            require_once("vues/header.php");
-                            require("vues/article.php");
-                            require_once("vues/footer.php");
-                            break; 
-                        }
-                    
+                if($date != "" && $titre != "" && $texte != ""&& $rubrique != "" && $visuel != "" && is_numeric($idArticle))
+                { 
+                    // Si tout est ok, on va chercher notre fonction pour updater les nouvelles informations dans la table
+                    $resultat = modifieArticle($date, $titre, $texte, $visuel, $idArticle, $rubrique);
+                    $lireArticle = articleId($idArticle);
+                    if(mysqli_num_rows($lireArticle) > 0)
+                    {
+                        $titre = "Lire article";
+                        require_once("vues/header.php");
+                        require("vues/article.php");
+                        require_once("vues/footer.php");
+                        break; 
                     }
-            
-            else 
-            {
-                header("Location: index.php?commande=formModifArticle&idArticle=$idArticle&message=Veuillez remplir correctement les champs.");
-                die();
-            } 
-            break;
-        }
+                }
+                else 
+                {
+                    header("Location: index.php?commande=formModifArticle&idArticle=$idArticle&message=Veuillez remplir correctement les champs.");
+                    die();
+                } 
+            }
+        break;
         case "formAjoutArticle":
+            // on crée les valeurs vides dont on a besoin pour le formulaire au cas où celui-ci soit invalide. S'il y a des request, la valeur prend celle du request pour retourner dans le formulaire d'ajout
             $date = "";
             $titre = "";
             $texte = "";
             $visuel = "";
             $idJournaliste = "";
             $rubrique = "";
-            //au cas ou le formulaire a déjà été rempli (il était invalide)
             if(isset($_REQUEST["date"])) 
                 $date = $_REQUEST["date"]; 
             if(isset($_REQUEST["titre"])) 
@@ -148,9 +149,8 @@
             require_once("vues/header.php");
             require("vues/formAjoutArticle.php");
             require_once("vues/footer.php");
-            break; 
+        break; 
         case "ajoutArticle":
-            //procéder à l'insertion et la validation
             if(isset($_REQUEST["date"], $_REQUEST["titre"], $_REQUEST["texte"], $_REQUEST["visuel"], $_REQUEST["rubrique"]))
             { 
                 $date = trim($_REQUEST["date"]);
@@ -161,44 +161,46 @@
                 $rubrique = trim($_REQUEST["rubrique"]);
                 if($date != "" && $titre != "" && $texte != "" && $rubrique != "" && $visuel != "" && $idJournaliste != "" && is_numeric($idJournaliste))
                 {
-                    //insertion
                     $resultat = ajoutArticle($date, $titre, $texte, $visuel, $idJournaliste, $rubrique);
                     if($resultat)
-            afficheArticleAccueil("Ajout réussie.");
+                        afficheArticleAccueil("Ajout réussie.");
                     else 
-                    afficheArticleAccueil("Aucun ajout effectué.");
-             
+                        afficheArticleAccueil("Aucun ajout effectué.");
                } 
-            else 
-            {
-                header("Location: index.php?commande=formAjoutArticle&message=Veuillez remplir correctement les champs.&date=$date&titre=$titre&texte=$texte&visuel=$visuel");
-                die();
-            } 
+                else 
+                {
+                    header("Location: index.php?commande=formAjoutArticle&message=Veuillez remplir correctement les champs.&date=$date&titre=$titre&texte=$texte&visuel=$visuel");
+                    die();
+                } 
             }
-            break;
+        break;
         case "supArticle": 
+            // on vérifie si on a bien un request id
             if(!isset($_GET["idArticle"]) || !is_numeric($_GET["idArticle"]))
             {
                 header("Location: index.php");
                 die();
             }
+            // On va chercher notre fonction pour supprimer l'article
             $resultat = supArticle($_GET["idArticle"]);
             if($resultat)
-            afficheArticleAccueil("Suppression réussie.");
-                    else 
-                    afficheArticleAccueil("Aucune suppression effectuée.");
-            break;  
+                afficheArticleAccueil("Suppression réussie.");
+            else 
+                afficheArticleAccueil("Aucune suppression effectuée.");
+        break;  
         case "rechercheArticle":
+            // Si une personne clique sur recherche sans inscrire de valeur, ou s'il n'y a pas de get recherche, alors on redirige vers la page d'accueil avec un message
             if(!isset($_GET["recherche"]) || $_GET["recherche"] == "")
             {
                 header("Location: index.php?commande=accueil&message=Vous n'avez rien inscrit dans le champ de recherche.");
                 die();
             }
+            // On va chercher notre fonction de recherche avec en parametre la valeur recherchée
             $recherche = $_GET["recherche"];
             $rechercheArticle = rechercheArticle($_GET["recherche"]);
+            // Si on a au moins un résultat alors on affiche la recherche, autrement on redirige vers la page d'accueil avec un message informant qu'il n'y a rien dans la base
             if(mysqli_num_rows($rechercheArticle) > 0)
-            {
-                
+            { 
                 $titre = "Rechercher un article"; 
                 require_once("vues/header.php");
                 require("vues/rechercheArticle.php");
@@ -209,12 +211,13 @@
                 header("Location: index.php?commande=accueil&message=Recherche Non fructueuse.");
                 die();
             } 
-            break; 
-        
+        break; 
+        // Si pas de commande, alors on redirige vers index
         default:
             header("Location: index.php");
             die();
     } 
+    // Création d'une fonction qui affiche les articles dans la page d'accueil car plusieurs fois demandée
     function afficheArticleAccueil($message = "")
     {
         $listeArticles = obtenirArticles();
@@ -224,6 +227,5 @@
         require_once("vues/header.php");
         require("vues/accueil.php");
         require_once("vues/footer.php");
-    }
-    
+    } 
 ?>
--- modele.php
+++ modele.php
@@ -2,7 +2,6 @@
 /*
     Modele : Fichier avec infos connexion et les fonctions qui font appel à la base uiquement. Les autres fonctions vont dans le controleur (index)
 */
-    
     //Connexion local
     /*
         define("SERVER", "localhost");
@@ -18,12 +17,12 @@
     
     function connectDB()
     {
-        //se connecter à la base de données
+        //On vérifie que la connexion est ok autrement on affiche les erreurs
         $c = mysqli_connect(SERVER, USERNAME, PASSWORD, DBNAME);
 
         if(!$c)
             die("Erreur de connexion. MySQLi : " . mysqli_connect_error());
-        //s'assurer que la connexion traite le UTF8
+        // On met tut en UTF8
         mysqli_query($c, "SET NAMES 'utf8'");
         return $c;
     }
@@ -36,50 +35,41 @@
         $requete = "SELECT s2tp2_articles.id, titre, texte, visuel, date, idJournaliste, rubrique FROM s2tp2_articles
         INNER JOIN s2tp2_journalistes ON s2tp2_journalistes.id = s2tp2_articles.idJournaliste
         ORDER BY s2tp2_articles.date DESC limit 3,30";
-        //exécuter avec mysqli_query
         $resultats = mysqli_query($connexion, $requete);
         return $resultats;
     }
     function obtenirDernierArticle()
     {
-        // Info pour se connecter
         global $connexion;
         $requete = "SELECT s2tp2_articles.id, titre, texte, visuel, date, idJournaliste, rubrique FROM s2tp2_articles
         INNER JOIN s2tp2_journalistes ON s2tp2_journalistes.id = s2tp2_articles.idJournaliste
         ORDER BY s2tp2_articles.date DESC limit 1";
-        //exécuter avec mysqli_query
         $resultats = mysqli_query($connexion, $requete);
         return $resultats;
     }
     function obtenir2ArticlesUne()
     {
-        // Info pour se connecter
         global $connexion;
         $requete = "SELECT s2tp2_articles.id, titre, texte, visuel, date, idJournaliste, rubrique FROM s2tp2_articles
         INNER JOIN s2tp2_journalistes ON s2tp2_journalistes.id = s2tp2_articles.idJournaliste
         ORDER BY s2tp2_articles.date DESC limit 1,2";
-        //exécuter avec mysqli_query
         $resultats = mysqli_query($connexion, $requete);
         return $resultats;
     }
     function obtenirRubriques()
     {
-        // Info pour se connecter
         global $connexion;
         $requete = "SELECT rubrique FROM s2tp2_rubriques
         ORDER BY rubrique";
-        //exécuter avec mysqli_query
         $resultats = mysqli_query($connexion, $requete);
         return $resultats;
     }
     function articleId($id)
     {
-        // Info pour se connecter
         global $connexion;
         $requete = "SELECT s2tp2_articles.id AS articleId, titre, texte, visuel, rubrique, date, idJournaliste, CONCAT(prenom,' ', nom) AS journaliste, identifiant FROM s2tp2_articles
         INNER JOIN s2tp2_journalistes ON s2tp2_journalistes.id = s2tp2_articles.idJournaliste
         WHERE s2tp2_articles.id=$id";
-        //exécuter avec mysqli_query
         $resultats = mysqli_query($connexion, $requete);
         return $resultats;
     }
@@ -88,41 +78,30 @@
         global $connexion;
         //on prépare la requête en mettant des ? à la place des paramètres qui viennent de l'usager
         $requete = "UPDATE s2tp2_articles SET date = ?, titre = ?,  texte = ?, visuel = ?, rubrique = ? WHERE id = ?";
-
-        //on prépare la requête
         $reqPrep = mysqli_prepare($connexion, $requete);
         //si la requête préparée fonctionne 
         if($reqPrep)
         { 
-            //faire le lien
+            // Si tout est ok, alors on donne les valeurs à chaque ?. S = texte - i = numerique
             mysqli_stmt_bind_param($reqPrep, "sssssi", $date, $titre,  $texte, $visuel, $rubrique, $articleId);
             //exécute la requête
             mysqli_stmt_execute($reqPrep);
-        
             if(mysqli_stmt_affected_rows($reqPrep) >= 1)
                 return true;
             else 
                 return false;
-         } 
+        } 
     }
     function ajoutArticle($date, $titre, $texte, $visuel, $idJournaliste, $rubrique)
     {
-        //obtenir la connexion définie plus haut (à l'extérieur de la fonction)
         global $connexion;
-
-        //on prépare la requête en mettant des ? à la place des paramètres qui viennent de l'usager
         $requete = "INSERT INTO s2tp2_articles(date, titre, texte, visuel, idJournaliste, rubrique) VALUES (?, ?, ?, ?, ?, ?)";
-
-        //on prépare la requête
         $reqPrep = mysqli_prepare($connexion, $requete);
         //si la requête préparée fonctionne 
         if($reqPrep)
         {
-            //faire le lien
             mysqli_stmt_bind_param($reqPrep, "ssssis", $date, $titre, $texte, $visuel, $idJournaliste, $rubrique);
-            //exécute la requête
             mysqli_stmt_execute($reqPrep);
-        
             if(mysqli_stmt_affected_rows($reqPrep) >= 1)
                 return true;
             else 
@@ -131,14 +110,9 @@
     }
     function supArticle($id)
     {
-        //obtenir la connexion définie plus haut (à l'extérieur de la fonction)
         global $connexion;
-
-        //déclarer la requête SQL à exécuter (TESTER DANS PHPMYADMIN AVANT)
         $requete = "DELETE FROM s2tp2_articles WHERE id = $id";
-        //exécuter avec mysqli_query
         $resultats = mysqli_query($connexion, $requete);
-        
         if(mysqli_affected_rows($connexion) > 0)
             return true;
         else 
@@ -146,29 +120,17 @@
     }
     function rechercheArticle($rechercheFaite)
     {
-        //obtenir la connexion définie plus haut (à l'extérieur de la fonction)
         global $connexion;
-
         $recherche = "%" . $rechercheFaite . "%";
-
-        //on prépare la requête en mettant des ? à la place des paramètres qui viennent de l'usager
         $requete = "SELECT s2tp2_articles.id, titre, texte, visuel, date, idJournaliste FROM s2tp2_articles
         INNER JOIN s2tp2_journalistes ON s2tp2_journalistes.id = s2tp2_articles.idJournaliste
-        
         WHERE titre like ? OR texte like ? OR rubrique like ?
         ORDER BY s2tp2_articles.date DESC";
-
-        //on prépare la requête
         $reqPrep = mysqli_prepare($connexion, $requete);
-        //si la requête préparée fonctionne 
         if($reqPrep)
         {
-            //faire le lien
             mysqli_stmt_bind_param($reqPrep, "sss",  $recherche, $recherche, $recherche);
-            //exécute la requête
             mysqli_stmt_execute($reqPrep);
-        
-            //obtenir le résultat
             $resultats = mysqli_stmt_get_result($reqPrep);
         }
         return $resultats;
