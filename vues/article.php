b9d64f6a7327814d2639e8452576e7d9847f5763
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-01-09 18:12:00 -0500

    Mise en place du preg_replace pour les articles pour que le htmlspecialchars fonctionne

--- vues/article.php
+++ vues/article.php
@@ -4,10 +4,11 @@
         <?php 
             while($rangee = mysqli_fetch_assoc($lireArticle))
             {
+                $texte = htmlspecialchars($rangee["texte"]) ;
                 echo "<p class=\"date\">" . htmlspecialchars($rangee["date"]) . " - ". htmlspecialchars(ucfirst($rangee["rubrique"])) . "</p>";
                 echo "<h1>" . htmlspecialchars($rangee["titre"]) . "</h1>";
                 echo "<img src=\"assets/img/articles/". htmlspecialchars($rangee["visuel"])."\" alt=\"". htmlspecialchars($rangee["titre"]) ."\">";
-                echo "<p>" . $rangee["texte"] . "</p>";
+                echo "<p>" .preg_replace('#&lt;(/?(?:br|h1|h3|ul|li|ol))&gt;#', '<\1>', $texte) . "</p>";
                 echo "<p class=\"auteur\">Journaliste : " . htmlspecialchars($rangee["journaliste"]) . "</p>";
                 echo "<div class=\"iconeEdition\">
                 <span class=\"material-symbols-outlined\"><a href=\"index.php?commande=supArticle&idArticle=".$rangee["articleId"]."\">delete</a></span>
