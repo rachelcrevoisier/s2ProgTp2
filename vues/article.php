201c2483a9acc1f54373e1f9504ff4eaef38b687
rachelcrevoisier <rachelcrevoisier@gmail.com>
2023-01-17 09:24:27 -0500

    Mise à jour de la session pour les modifs et sup de l'article

--- vues/article.php
+++ vues/article.php
@@ -8,12 +8,16 @@
                 echo "<p class=\"date\">" . htmlspecialchars($rangee["date"]) . " - ". htmlspecialchars(ucfirst($rangee["rubrique"])) . "</p>";
                 echo "<h1>" . htmlspecialchars($rangee["titre"]) . "</h1>";
                 echo "<img src=\"assets/img/articles/". htmlspecialchars($rangee["visuel"])."\" alt=\"". htmlspecialchars($rangee["titre"]) ."\">";
-                echo "<p>" .preg_replace('#&lt;(/?(?:br|h1|h3|ul|li|ol))&gt;#', '<\1>', $texte) . "</p>";
+                echo "<p>" .preg_replace('#&lt;(/?(?:br|h1|h3|p|ul|li|ol|i))&gt;#', '<\1>', $texte) . "</p>";
                 echo "<p class=\"auteur\">Journaliste : " . htmlspecialchars($rangee["journaliste"]) . "</p>";
-                echo "<div class=\"iconeEdition\">
-                <span class=\"material-symbols-outlined\"><a href=\"index.php?commande=supArticle&idArticle=".$rangee["articleId"]."\">delete</a></span>
-                <span class=\"material-symbols-outlined\"><a href=\"index.php?commande=formModifArticle&idArticle=".$rangee["articleId"]."\">edit_note</a></span>
-                </div>";
+                if(isset($_SESSION["username"]) && $rangee["idJournaliste"]==$_SESSION["username"])
+                    {
+                        echo "<div class=\"iconeEdition\">
+                        <span class=\"material-symbols-outlined\"><a href=\"index.php?commande=supArticle&idArticle=".$rangee["articleId"]."&idJournaliste=".$rangee["idJournaliste"]."\">delete</a></span>
+                        <span class=\"material-symbols-outlined\"><a href=\"index.php?commande=formModifArticle&idArticle=".$rangee["articleId"]."&idJournaliste=".$rangee["idJournaliste"]."\">edit_note</a></span>
+                        </div>";
+                    }
+
             }
         ?>
     </section>
